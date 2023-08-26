b40e6c2168226c720aed533a58795b3fd9eef8b1
ChauveSouris <desseobscurite@gmail.com>
2023-08-26 23:41:49 +0300

    Initial Commit

---
+++ ColorizeApp/ColorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizeAppApp.swift
+//  ColorizeApp
+//
+//  Created by Елизавета Медведева on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
