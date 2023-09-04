640aa8c9aeed400f9a9121c89a5c86d2309703c7
ChauveSouris <desseobscurite@gmail.com>
2023-09-04 21:17:34 +0300

    Created ColorView

--- ColorizeApp/ColorView.swift
+++ ColorizeApp/ColorView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct ColorView: View {
+    
+    let red: Double
+    let green: Double
+    let blue: Double
+    
     var body: some View {
-        
+        Color(red: red / 255, green: green / 255, blue: blue / 255)
+            .frame(height: 150)
+            .cornerRadius(15)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.black, lineWidth: 2)
+            )
     }
 }
 
 struct ColorView_Previews: PreviewProvider {
     static var previews: some View {
-    
+    ColorView(red: 50, green: 50, blue: 50)
     }
 }
