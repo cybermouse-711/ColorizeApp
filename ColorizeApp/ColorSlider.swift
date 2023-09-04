16566e6aec237a569c1028fc741f5261305046a2
ChauveSouris <desseobscurite@gmail.com>
2023-09-04 21:56:42 +0300

    Created Slider

--- ColorizeApp/ColorSlider.swift
+++ ColorizeApp/ColorSlider.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct ColorSlider: View {
+
+    @Binding var value: Double
+    @State private var text = ""
+    @State private var alert = false
+    
+    let color: Color
+    
     var body: some View {
-        
+        HStack{
+            Text(value.formatted())
+                .frame(width: 30, alignment: .leading)
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
+                .onChange(of: value) { newValue in
+                    text = newValue.formatted()
+                }
+            ColorTextField(text: $text, value: $value, action: checkValue)
+                .alert("Wrong Format", isPresented: $alert, actions: {}) {
+                    Text("Format 0...255")
+                }
+        }
+        .onAppear{
+            text = value.formatted()
+        }
+    }
+    
+    private func checkValue() {
+        if let value = Int(text), (0...255).contains(value) {
+            self.value = Double(text)
+            return
+        }
+        alert.toggle()
+        value = 0
+        text = "0"
     }
 }
 
 struct ColorSlider_Previews: PreviewProvider {
     static var previews: some View {
-
+        ColorSlider(value: 50, color: .red)
     }
 }
