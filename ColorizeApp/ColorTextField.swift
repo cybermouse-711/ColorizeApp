4dc0a8884ab948b2dddea7d43949977fbab07adf
ChauveSouris <desseobscurite@gmail.com>
2023-09-04 21:28:26 +0300

    Created TextField

--- ColorizeApp/ColorTextField.swift
+++ ColorizeApp/ColorTextField.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ColorTextField: View {
+    
+    @Binding var text: String
+    @Binding var value: Double
+    
+    let action: () -> Void
+    
     var body: some View {
-        
+        TextField("0", text: $text) { _ in
+            withAnimation {
+                action()
+            }
+        }
+        .frame(width: 50, alignment: .trailing)
+        .multilineTextAlignment(.trailing)
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.decimalPad)
     }
 }
 
 struct ColorTextField_Previews: PreviewProvider {
     static var previews: some View {
-        
+        ColorTextField(text: .constant("0"), value: .constant(0), action: {})
     }
 }
 
